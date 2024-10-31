4017035c67113392449502d231433990b39dfda8
Harper Reed <harper@nata2.org>
2024-10-30 21:44:57 -0500

    Add exposure fusion and tone mapping methods to run.sh 🎉✨
    - Added execution command for `exposure-fusion` method with output file `landscape_hdr-exposure-fusion.jpg` 🎨
    - Introduced a second command for `tone-mapping` method with output file `landscape_hdr-tone-mapping.jpg` 🖌️
    
    These changes increase the functionality of the `run.sh` script by allowing users to apply two different HDR methods (exposure fusion and tone mapping) to the specified input images, giving them more options for generating HDR outputs. This is especially useful for users looking for varied results from their images without changing the execution context! 🌈📸

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-go run ./cmd/hdarrrr -low examples/landscape_low.jpeg -mid examples/landscape_mid.jpeg -high examples/landscape_high.jpeg -output examples/landscape_hdr.jpg
+go run ./cmd/hdarrrr -method exposure-fusion -low examples/landscape_low.jpeg -mid examples/landscape_mid.jpeg -high examples/landscape_high.jpeg -output examples/landscape_hdr-exposure-fusion.jpg
+
+go run ./cmd/hdarrrr -method tone-mapping -low examples/landscape_low.jpeg -mid examples/landscape_mid.jpeg -high examples/landscape_high.jpeg -output examples/landscape_hdr-tone-mapping.jpg
